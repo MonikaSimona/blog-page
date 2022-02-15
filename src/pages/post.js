e98e3135aa4d4e97a1f22a6029e25420cc453f80
Simona Simonovska <simona.simonovska@atis.al>
2022-02-15 16:19:25 +0100

    initial commit

---
+++ src/pages/post.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Redirect } from "react-router-dom"
+import Markdown from "react-markdown"
+import Layout from "../components/layout"
+import postlist from "../posts.json"
+import "./pages.css"
+
+const Post = (props) => {
+    const validId = parseInt(props.match.params.id)
+    if (!validId) {
+        return <Redirect to="/404" />
+    }
+    const fetchedPost = {}
+    let postExists = false
+    postlist.forEach((post, i) => {
+        if (validId === post.id) {
+            fetchedPost.title = post.title ? post.title : "No title given"
+            fetchedPost.date = post.date ? post.date : "No date given"
+            fetchedPost.author = post.author ? post.author : "No author given"
+            fetchedPost.description = post.description ? post.description : "No description given"
+            fetchedPost.content = post.content ? post.content : "No content given"
+            fetchedPost.image = post.image ? post.image : "No image given"
+            postExists = true
+        }
+    })
+    if (postExists === false) {
+        return <Redirect to="/404" />
+    }
+    return (
+        <Layout>
+            <div className="post">
+                <h2>{fetchedPost.title}</h2>
+                {fetchedPost.image && <img src={fetchedPost.image} width="500" height="500" alt="" />}
+                <p>{fetchedPost.description}</p>
+                <small>Published on {fetchedPost.date} by {fetchedPost.author}</small>
+                <hr />
+                <Markdown source={fetchedPost.content} escapeHtml={false} />
+            </div>
+        </Layout>
+    )
+}
+
+export default Post
