146c4760ac39854dfa445ef402aca2ba9765cc74
Simona Simonovska <simona.simonovska@atis.al>
2022-02-23 16:23:10 +0100

    all blogs page

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react'
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import BlogCard from '../components/BlogCard'
@@ -21,11 +22,22 @@ const BlogsPage = () => {
 
         <div className="all-blogs-page-wrapper">
 
-            <div className="title-wrapper">
-                <h1 className='page-title'>{title}</h1>
-                <img src={require("../assets/images/titleStroke.svg")} alt="" />
+            <div className="all-blogs-hero">
+                <img src={require("../assets/images/allBlogHeroBg.png")} alt="" />
+                <div className="title-wrapper">
+                    <h1 className='page-title'>{title}</h1>
+                    <img src={require("../assets/images/titleStroke.svg")} alt="" />
+                </div>
             </div>
             <div className="container">
+                <div className="search-bar-wrapper">
+                    <label htmlFor="search">
+                        <Icon icon="bytesize:search" color="#7b726b" className='search-icon' width={28} />
+                    </label>
+                    <img src={require("../assets/images/searchBar.svg")} alt="" />
+                    <input type="text" id='search' />
+
+                </div>
                 <div className="blog-cards-wrapper">
                     {blogsFromCategory.length > 0 ? blogsFromCategory.map((post) => (
 
