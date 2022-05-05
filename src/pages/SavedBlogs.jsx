e24bf48d72d09ed8eed835f5551c6c6d98593750
Simona Simonovska <simona.simonovska@atis.al>
2022-05-05 16:55:51 +0200

    cards for saved blogs, loading spinner

--- src/pages/SavedBlogs.jsx
+++ src/pages/SavedBlogs.jsx
@@ -5,17 +5,21 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import postlist from "../posts.json"
 import { getAuth } from "firebase/auth";
+import BlogCard from "../components/BlogCard";
+import { Icon } from "@iconify/react";
+import { arrayRemove, doc, updateDoc } from "firebase/firestore";
+import { FIREBASE_DB } from "../firebase";
+import { SpinnerDotted, SpinnerRoundFilled } from "spinners-react";
 
 const SavedBlogs = () => {
   const { loggedIn, user } = useSelector((state) => state.user);
   const [savedBlogs, setSavedBlogs] = useState([])
-
+  const [registerBlogId, setRegisterBlogId] = useState()
   const auth = getAuth();
   // console.log("AUTH", user)
+  var temp = [];
 
-
-  useEffect(() => {
-    var temp = [];
+  const fetchSavedBlogs = () => {
     if (user) {
       postlist.forEach((item) => {
         user.savedBlogs.forEach((saved) => {
@@ -28,15 +32,49 @@ const SavedBlogs = () => {
       setSavedBlogs(temp)
       // console.log(user.savedBlogs)
     }
+  }
+
+  useEffect(() => {
+    fetchSavedBlogs()
   }, [user?.savedBlogs])
+
+  const handleRemoveSaved = (blogId) => {
+    const users = doc(FIREBASE_DB, "users", user.id);
+    updateDoc(users, {
+      savedBlogs: arrayRemove(blogId)
+    }).then((res) => {
+      console.log('success', res)
+    }).catch((err) => {
+      console.log('error', err)
+    })
+    setRegisterBlogId(blogId)
+
+
+  }
+  useEffect(() => {
+    fetchSavedBlogs()
+
+  }, [registerBlogId])
 
 
   return <div className="container">
     {user ? savedBlogs?.length > 0 ? (
-      <div>
-        {savedBlogs.map((item) => (
-          <p>{item.title}</p>
-        ))}
+      <div className="saved-blogs">
+
+        <h3 className="user-data-title" >
+          Saved Blogs
+        </h3>
+        <div className="saved-blogs-wrapper">
+          {savedBlogs.map((item) => (
+            <div className="single-blog" key={item.id} >
+              <BlogCard id={item.id} title={item.title} image={item.image} date={item.date} />
+              <div className="additional-card-footer">
+                <Icon icon="fluent:delete-28-regular" onClick={() => handleRemoveSaved(item.id)} />
+                <Icon icon="fluent:share-48-regular" />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
     ) : (
@@ -52,7 +90,11 @@ const SavedBlogs = () => {
         </p>
         <img src={require("../assets/images/savedBlogsDecoration.svg")} alt="" />
       </div>
-    ) : "no user"}
+    ) : (
+      <div className="loading-wrapper">
+        <SpinnerRoundFilled size={200} color="#d63037" secondaryColor="#3b89a0" />
+      </div>
+    )}
   </div>;
 };
 
