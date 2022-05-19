3a670d7c25764c42f5a6cd9f2023f544c679cc09
Simona Simonovska <simona.simonovska@atis.al>
2022-05-19 10:13:31 +0200

    back to top button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,55 +24,60 @@ const Header = ({ scrollElementRef, handleLoginModal }) => {
 
 
   return (
-    <div className="header-wrapper">
-      <div className="container">
-        <div className="header">
-          <Link to="/" className="logo">
-            <img src={require("../assets/images/logo.svg")} alt="" />
-          </Link>
-          <div className="header-right-items">
-            <NavLink to="/about" className="nav-item">
-              About
-            </NavLink>
-            {!user && (
-              <span className="nav-item" onClick={handleLoginModal}>
-                Login
-              </span>
-            )}
-            {!!user && (
-              <div className="drop-down-item" onClick={() => setOpenMenu(!openMenu)}>
-                {user.profileImage ? <img src={user.profileImage} alt="" /> : <Icon icon="carbon:user-avatar-filled" fontSize={28} />}
-                <p>{user.name}</p>
-                <ul className={`drop-down-content ${openMenu && "open"}`}>
-                  <li> <NavLink to="/profile">  <Icon icon="healthicons:ui-user-profile-outline" fontSize={18} />  Profile</NavLink> </li>
-                  <li> <NavLink to="/saved-blogs"> <Icon icon="fluent:copy-20-regular" fontSize={18} />Saved blogs</NavLink> </li>
-                  <li> <span onClick={
-                    () => { dispatch(removeUser()); signOut(auth); navigate("/") }
-                  }> <Icon icon="bi:door-open" fontSize={18} />Log out</span> </li>
-                </ul>
-              </div>
-            )}
-            {!user && (
-              <div className="cta-wrapper">
-                <span
-                  onClick={() => {
-                    executeScroll();
-                  }}
-                  className="cta"
-                >
-                  Become a member
+    <>
+      <div className="scroll-to-top" onClick={() => window.scrollTo(0, 0)}>
+        <img src={require("../assets/images/fatarow.svg")} alt="" />
+      </div>
+      <div className="header-wrapper">
+        <div className="container">
+          <div className="header">
+            <Link to="/" className="logo">
+              <img src={require("../assets/images/logo.svg")} alt="" />
+            </Link>
+            <div className="header-right-items">
+              <NavLink to="/about" className="nav-item">
+                About
+              </NavLink>
+              {!user && (
+                <span className="nav-item" onClick={handleLoginModal}>
+                  Login
                 </span>
-                <img
-                  className="arrow"
-                  src={require("../assets/images/fatarow.svg")}
-                  alt=""
-                />
-              </div>
-            )}
+              )}
+              {!!user && (
+                <div className="drop-down-item" onClick={() => setOpenMenu(!openMenu)}>
+                  {user.profileImage ? <img src={user.profileImage} alt="" /> : <Icon icon="carbon:user-avatar-filled" fontSize={28} />}
+                  <p>{user.name}</p>
+                  <ul className={`drop-down-content ${openMenu && "open"}`}>
+                    <li> <NavLink to="/profile">  <Icon icon="healthicons:ui-user-profile-outline" fontSize={18} />  Profile</NavLink> </li>
+                    <li> <NavLink to="/saved-blogs"> <Icon icon="fluent:copy-20-regular" fontSize={18} />Saved blogs</NavLink> </li>
+                    <li> <span onClick={
+                      () => { dispatch(removeUser()); signOut(auth); navigate("/") }
+                    }> <Icon icon="bi:door-open" fontSize={18} />Log out</span> </li>
+                  </ul>
+                </div>
+              )}
+              {!user && (
+                <div className="cta-wrapper">
+                  <span
+                    onClick={() => {
+                      executeScroll();
+                    }}
+                    className="cta"
+                  >
+                    Become a member
+                  </span>
+                  <img
+                    className="arrow"
+                    src={require("../assets/images/fatarow.svg")}
+                    alt=""
+                  />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
